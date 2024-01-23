ecea54365673d6b04a8bb88fc2e9f776b46ae0bd
massivebird <gdrakemail@gmail.com>
2024-01-23 00:56:41 -0500

    changes: function names to exclude numbers

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ pub fn run(left: usize, right: usize) -> usize {
     left + right
 }
 
-fn to_1s_complement(n: i32) -> i32 {
+fn to_ones_complement(n: i32) -> i32 {
     if n.is_positive() || n == 0 { return n; }
 
     let num_unsigned_bits = format!("{:b}", !n).len();
@@ -23,9 +23,9 @@ fn to_1s_complement(n: i32) -> i32 {
     i32::from_str_radix(&bit_string_1c, 2).unwrap() - 1
 }
 
-fn to_2s_complement(n: i32) -> i32 {
+fn to_twos_complement(n: i32) -> i32 {
     if n.is_negative() {
-        return to_1s_complement(n) + 1;
+        return to_ones_complement(n) + 1;
     }
     n
 }
@@ -40,47 +40,47 @@ mod tests {
     }
 
     #[test]
-    fn test_1s_complement_zero() {
-        assert_eq!(to_1s_complement(0), 0b0);
+    fn test_ones_complement_zero() {
+        assert_eq!(to_ones_complement(0), 0b0);
     }
 
     #[test]
-    fn test_1s_complement_p_0() {
-        assert_eq!(to_1s_complement(25), 0b0011001);
+    fn test_ones_complement_p_0() {
+        assert_eq!(to_ones_complement(25), 0b0011001);
     }
 
     #[test]
-    fn test_1s_complement_p_1() {
-        assert_eq!(to_1s_complement(35), 0b0100011);
+    fn test_ones_complement_p_1() {
+        assert_eq!(to_ones_complement(35), 0b0100011);
     }
 
     #[test]
-    fn test_1s_complement_n_0() {
-        assert_eq!(to_1s_complement(-22), 0b10_1001);
+    fn test_ones_complement_n_0() {
+        assert_eq!(to_ones_complement(-22), 0b10_1001);
     }
 
     #[test]
-    fn test_1s_complement_n_1() {
-        assert_eq!(to_1s_complement(-42), 0b101_0101);
+    fn test_ones_complement_n_1() {
+        assert_eq!(to_ones_complement(-42), 0b101_0101);
     }
 
     #[test]
-    fn test_1s_complement_n_2() {
-        assert_eq!(to_1s_complement(-35), 0b101_1100);
+    fn test_ones_complement_n_2() {
+        assert_eq!(to_ones_complement(-35), 0b101_1100);
     }
 
     #[test]
-    fn test_1s_complement_n_3() {
-        assert_eq!(to_1s_complement(-90), 0b10100101);
+    fn test_ones_complement_n_3() {
+        assert_eq!(to_ones_complement(-90), 0b10100101);
     }
 
     #[test]
-    fn test_2s_complement_zero() {
-        assert_eq!(to_2s_complement(0), 0b0);
+    fn test_twos_complement_zero() {
+        assert_eq!(to_twos_complement(0), 0b0);
     }
 
     #[test]
-    fn test_2s_complement_n_0() {
-        assert_eq!(to_2s_complement(-90), 0b10100110);
+    fn test_twos_complement_n_0() {
+        assert_eq!(to_twos_complement(-90), 0b10100110);
     }
 }
