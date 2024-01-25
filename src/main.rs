e81f704bdd8b0be74d463dc136b107da5fd25aaa
massivebird <gdrakemail@gmail.com>
2024-01-24 21:38:38 -0500

    feat: clap

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,20 @@ use binary_conversions::{
     to_twos_complement,
     to_excess_64,
 };
+use clap::Arg;
 
 fn main() {
-    let n: i32 = 35;
+    let matches = clap::command!()
+        .arg(Arg::new("n")
+            .required(true)
+            .value_name("NUM")
+            .value_parser(clap::value_parser!(i32))
+            .help("Specify the decimal number to convert")
+        )
+        .get_matches();
+
+    let n: i32 = *matches.get_one("n").unwrap();
+
     println!("Evaluating decimal {n}...");
     println!("1's complement: {:#b}", to_ones_complement(n));
     println!("2's complement: {:#b}", to_twos_complement(n));
