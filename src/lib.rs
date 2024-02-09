4b3e6050816afea53af7da3f2e276d04acb27a17
massivebird <gdrakemail@gmail.com>
2024-02-09 18:25:36 -0500

    style: reorders functions

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,7 @@
 //! 3) Two's complement
 //! 4) Excess-128
 
+/// Non-interactive run. Can be omitted from Rust Playground.
 pub fn run(n: i128) {
     let excess_output = |e, n| {
         match to_excess(e, n) {
@@ -29,7 +30,28 @@ pub fn run(n: i128) {
     println!("Excess-128:        {}", excess_output(128, n));
 }
 
-pub fn run_to_binary() {
+/// An interactive version of the program.
+/// Rust Playground does not support command line arguments, so
+/// an interactive mode is necessary.
+pub fn main() {
+    println!("Converting (1) decimal to binary, or (2) binary to decimal?");
+    let mode: i128 = loop {
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        if let Ok(n) = input.trim().parse::<i128>() {
+            if n != 1 && n != 2 { continue }
+            break n;
+        }
+    };
+
+    match mode {
+        1 => interactive_to_binary(),
+        2 => interactive_to_decimal(),
+        _ => unreachable!(),
+    }
+}
+
+pub fn interactive_to_binary() {
     println!("Enter a number to convert to binary:");
     let n: i128 = loop {
         let mut input = String::new();
@@ -47,15 +69,16 @@ pub fn run_to_binary() {
     };
 
     println!("Evaluating decimal {n}...");
-    println!("Unsigned:        {}", to_unsigned_unpadded(n));
-    println!("Ones complement: {}", to_ones_complement(n));
-    println!("Twos complement: {}", to_twos_complement(n));
+    // println!("Unsigned:         {}", to_unsigned_unpadded(n));
+    println!("Signed magnitude: {}", pad(8, &to_signed(n)));
+    println!("Ones complement:  {}", to_ones_complement(n));
+    println!("Twos complement:  {}", to_twos_complement(n));
     // println!("Excess-32:      {}", excess_output(32, n));
     // println!("Excess-64:      {}", excess_output(64, n));
-    println!("Excess-128:      {}", excess_output(128, n));
+    println!("Excess-128:       {}", excess_output(128, n));
 }
 
-fn run_to_decimal() {
+fn interactive_to_decimal() {
     println!("Enter a bit string to convert to decimal:");
     let bit_string: String = loop {
         let mut input = String::new();
@@ -64,7 +87,7 @@ fn run_to_decimal() {
         break input.trim().to_string();
     };
 
-    let ok_value_or_err_msg = |r: Result<i128, String>| {
+    let unpack = |r: Result<i128, String>| {
         match r {
             Ok(a) => a.to_string(),
             Err(msg) => msg,
@@ -72,11 +95,11 @@ fn run_to_decimal() {
     };
 
     println!("Evaluating bit string {bit_string} as different notations...");
-    println!("Unsigned:          {}", from_unsigned(&bit_string));
-    println!("Signed magnitude:  {}", from_signed(&bit_string));
-    println!("Ones complement:   {}", from_ones_complement(&bit_string));
-    println!("Twos complement:   {}", ok_value_or_err_msg(from_twos_complement(&bit_string)));
-    println!("Excess-128:        {}", ok_value_or_err_msg(from_excess_128(&bit_string)));
+    // println!("Unsigned:         {}", from_unsigned(&bit_string));
+    println!("Signed magnitude: {}", from_signed(&bit_string));
+    println!("Ones complement:  {}", from_ones_complement(&bit_string));
+    println!("Twos complement:  {}", unpack(from_twos_complement(&bit_string)));
+    println!("Excess-128:       {}", unpack(from_excess_128(&bit_string)));
 }
 
 /// Converts a decimal to its 8-bit signed binary form.
@@ -88,113 +111,37 @@ fn to_signed(n: i128) -> String {
     format!("1{}", pad(7, &to_unsigned_unpadded(n)))
 }
 
-/// Converts a bit string in excess-128 form to its decimal value.
-fn from_excess_128(bit_string: &str) -> Result<i128, String> {
-    let maximum_bits = 8;
-    if bit_string.len() > maximum_bits {
-        return Err(format!("too many bits: maximum of {maximum_bits}"));
-    }
+/// Converts a decimal to its 8-bit unsigned binary form, which is NOT
+/// padded to any number of bits.
+fn to_unsigned_unpadded(n: i128) -> String {
+    let n = n.abs();
 
-    Ok(from_unsigned(&bit_string) - 128)
-}
+    if n == 0 { return "0".to_string(); }
 
-/// Pads a bit string so that it expresses the same value in 
-/// a specified number of bits.
-fn pad(num_bits: usize, bit_string: &str) -> String {
-    assert!(num_bits >= bit_string.len());
-
-    let padding = "0".repeat(num_bits - bit_string.len());
-    format!("{padding}{bit_string}")
-}
-
-/// Converts a bit string in signed form to its decimal value.
-fn from_signed(bit_string: &str) -> i128 {
-    let sign_bit = bit_string.chars().nth(0).unwrap();
-    if sign_bit == '0' { return from_unsigned(bit_string) }
-
-    let magnitude = &bit_string.chars().skip(1).collect::<String>();
-    -1 * from_unsigned(&magnitude)
-}
-
-/// Converts a bit string in twos complement form to its decimal value.
-fn from_twos_complement(bit_string: &str) -> Result<i128, String> {
-    let sign_bit = bit_string.chars().nth(0).unwrap();
-    // positive -> unchanged from unsigned form
-    if sign_bit == '0' { return Ok(from_unsigned(bit_string)) }
-
-    let magnitude = bit_string.chars().skip(1).collect::<String>();
-
-    // since we have to subtract 1 from this bit string,
-    // 10000 -> 01111 (overflow??)
-    if magnitude.chars().all(|c| c == '0') {
-        return Err("not enough bits".to_string())
-    }
-
-    let Some(position_of_smallest_one) = magnitude.rfind('1') else {
-        // do I need to do this? should it be done? is it ethical?
-        todo!();
-    };
-
-    // flip the magnitude AFTER subtracting one, which is done by
-    // flipping the smallest 1 bit, then flipping all lesser value bits!
-    let flipped_magnitude = flip_bits(&magnitude
-        .chars()
-        .enumerate()
-        .map(|(i, value)| {
-            if i == position_of_smallest_one { return '0' }
-            if i > position_of_smallest_one  { return '1' }
-            value
-        })
-        .collect::<String>()
-    );
-
-    return Ok(
-        -1 * from_unsigned(&flipped_magnitude)
-    );
-}
-
-/// Converts a bit string in ones complement form to its decimal value.
-fn from_ones_complement(bit_string: &str) -> i128 {
-    let sign_bit = bit_string.chars().nth(0).unwrap();
-    // positive -> unchanged from unsigned form
-    if sign_bit == '0' { return from_unsigned(bit_string) }
-
-    let magnitude = bit_string.chars().skip(1).collect::<String>();
-    let flipped_magnitude = flip_bits(&magnitude);
-
-    return -1 * from_unsigned(&flipped_magnitude);
-}
-
-/// Converts a bit string in unsigned form to its decimal value.
-fn from_unsigned(bit_string: &str) -> i128 {
-    bit_string
-        .chars()
-        .rev()
-        .enumerate()
-        .fold(0, |acc, (index, char)| {
-            if char == '0' { return acc; }
-            acc + 2i128.pow(index as u32)
-        })
-}
-
-/// An interactive version of the program.
-/// Rust Playground does not support command line arguments, so
-/// an interactive mode is necessary.
-pub fn main() {
-    println!("Converting (1) decimal to binary, or (2) binary to decimal?");
-    let mode: i128 = loop {
-        let mut input = String::new();
-        std::io::stdin().read_line(&mut input).unwrap();
-        if let Ok(n) = input.trim().parse::<i128>() {
-            if n != 1 && n != 2 { continue }
-            break n;
+    // let num_bits = 8;
+    let num_bits = {
+        let mut i: u32 = 0;
+        loop {
+            if 2i128.pow(i) > n { break i };
+            i += 1;
         }
     };
 
-    match mode {
-        1 => run_to_binary(),
-        2 => run_to_decimal(),
-        _ => unreachable!(),
+    {
+        let mut working_bit_string = String::new();
+
+        let mut remaining_value = n;
+
+        for place_value in (0..num_bits).rev().map(|v| 2i128.pow(v)) {
+            if place_value <= remaining_value {
+                working_bit_string.push('1');
+                remaining_value -= place_value;
+                continue;
+            }
+            working_bit_string.push('0');
+        }
+
+        working_bit_string
     }
 }
 
@@ -244,40 +191,6 @@ fn to_twos_complement(n: i128) -> String {
     format!("1{ones_comp_plus_one}")
 }
 
-/// Converts a decimal to its 8-bit unsigned binary form, which is NOT
-/// padded to any number of bits.
-fn to_unsigned_unpadded(n: i128) -> String {
-    let n = n.abs();
-
-    if n == 0 { return "0".to_string(); }
-
-    // let num_bits = 8;
-    let num_bits = {
-        let mut i: u32 = 0;
-        loop {
-            if 2i128.pow(i) > n { break i };
-            i += 1;
-        }
-    };
-
-    {
-        let mut working_bit_string = String::new();
-
-        let mut remaining_value = n;
-
-        for place_value in (0..num_bits).rev().map(|v| 2i128.pow(v)) {
-            if place_value <= remaining_value {
-                working_bit_string.push('1');
-                remaining_value -= place_value;
-                continue;
-            }
-            working_bit_string.push('0');
-        }
-
-        working_bit_string
-    }
-}
-
 /// Converts a value `n` to excess `e`.
 ///
 /// # Errors
@@ -297,6 +210,86 @@ pub fn to_excess(e: i128, n: i128) -> Result<String, String> {
     Ok(pad(8, &unpadded_bit_string))
 }
 
+/// Converts a bit string in unsigned form to its decimal value.
+fn from_unsigned(bit_string: &str) -> i128 {
+    bit_string
+        .chars()
+        .rev()
+        .enumerate()
+        .fold(0, |acc, (index, char)| {
+            if char == '0' { return acc; }
+            acc + 2i128.pow(index as u32)
+        })
+}
+
+/// Converts a bit string in signed form to its decimal value.
+fn from_signed(bit_string: &str) -> i128 {
+    let sign_bit = bit_string.chars().nth(0).unwrap();
+    if sign_bit == '0' { return from_unsigned(bit_string) }
+
+    let magnitude = &bit_string.chars().skip(1).collect::<String>();
+    -1 * from_unsigned(&magnitude)
+}
+
+/// Converts a bit string in ones complement form to its decimal value.
+fn from_ones_complement(bit_string: &str) -> i128 {
+    let sign_bit = bit_string.chars().nth(0).unwrap();
+    // positive -> unchanged from unsigned form
+    if sign_bit == '0' { return from_unsigned(bit_string) }
+
+    let magnitude = bit_string.chars().skip(1).collect::<String>();
+    let flipped_magnitude = flip_bits(&magnitude);
+
+    return -1 * from_unsigned(&flipped_magnitude);
+}
+
+/// Converts a bit string in twos complement form to its decimal value.
+fn from_twos_complement(bit_string: &str) -> Result<i128, String> {
+    let sign_bit = bit_string.chars().nth(0).unwrap();
+    // positive -> unchanged from unsigned form
+    if sign_bit == '0' { return Ok(from_unsigned(bit_string)) }
+
+    let magnitude = bit_string.chars().skip(1).collect::<String>();
+
+    // since we have to subtract 1 from this bit string,
+    // 10000 -> 01111 (overflow??)
+    if magnitude.chars().all(|c| c == '0') {
+        return Err("not enough bits".to_string())
+    }
+
+    let Some(position_of_smallest_one) = magnitude.rfind('1') else {
+        // do I need to do this? should it be done? is it ethical?
+        todo!();
+    };
+
+    // flip the magnitude AFTER subtracting one, which is done by
+    // flipping the smallest 1 bit, then flipping all lesser value bits!
+    let flipped_magnitude = flip_bits(&magnitude
+        .chars()
+        .enumerate()
+        .map(|(i, value)| {
+            if i == position_of_smallest_one { return '0' }
+            if i > position_of_smallest_one  { return '1' }
+            value
+        })
+        .collect::<String>()
+    );
+
+    return Ok(
+        -1 * from_unsigned(&flipped_magnitude)
+    );
+}
+
+/// Converts a bit string in excess-128 form to its decimal value.
+fn from_excess_128(bit_string: &str) -> Result<i128, String> {
+    let maximum_bits = 8;
+    if bit_string.len() > maximum_bits {
+        return Err(format!("too many bits: maximum of {maximum_bits}"));
+    }
+
+    Ok(from_unsigned(&bit_string) - 128)
+}
+
 /// Flips all bits in a bit string.
 fn flip_bits(bit_string: &str) -> String {
     bit_string.chars()
@@ -305,6 +298,15 @@ fn flip_bits(bit_string: &str) -> String {
             '0'
         })
         .collect::<String>()
+}
+
+/// Pads a bit string so that it expresses the same value in 
+/// a specified number of bits.
+fn pad(num_bits: usize, bit_string: &str) -> String {
+    assert!(num_bits >= bit_string.len());
+
+    let padding = "0".repeat(num_bits - bit_string.len());
+    format!("{padding}{bit_string}")
 }
 
 #[cfg(test)]
